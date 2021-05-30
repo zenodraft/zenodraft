7f694bd7e874f7a64414f935d260fb24f9ae4042
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2021-05-30 12:32:29 +0200

    finally does something

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,38 @@
 import * as dotenv from 'dotenv'
 import fetch from 'node-fetch'
+import { RequestInit } from 'node-fetch'
+import { URLSearchParams } from 'url'
+import { DepositionsResponse } from './zenodo-response-types'
 
 
 const create_new_empty_upload = async (api: string, access_token: string) => {
+    
+    const endpoint = '/deposit/depositions'
+    const method = 'POST'
+    const headers = {
+        'Authorization': `Bearer ${access_token}`,
+        'Content-Type': 'application/json'
+    }
+    const init: RequestInit = { method, headers, body: JSON.stringify({}) }
+    let response: any
     try {
-        const endpoint = '/deposit/depositions'
-        const init = {
-            body: {},
-            headers: {
-                'Authorization': `Bearer ${access_token}`,
-                'Content-type': 'application/json'
-            },
-            method: 'POST'
-        }
-        const response = await fetch(api + endpoint, init as any)
-        return await response.json()
+        response = await fetch(api + endpoint, init)
     } catch (e) {
-        console.log(e)
+        throw new Error(`Didn't get the expected response from ${api}${endpoint}: ${response.status} - ${response.statusText} `)
+    }
+
+    try {
+        return await response.json() as DepositionsResponse
+    } catch (e) {
+        throw new Error(`Something went wrong while retrieving the json. ${e}`)
     }
 }
 
 
 (async () => {
     const sandbox: boolean = true
-//    const conceptdoi: string = '10.5281/zenodo.831247'
     const access_token: string = dotenv.config().parsed!.ZENODO_SANDBOX_ACCESS_TOKEN
     const api: string = `https://${sandbox ? 'sandbox.' : ''}zenodo.org/api`
-    
     const response = await create_new_empty_upload(api, access_token)
     console.log(response)
-
 })()
