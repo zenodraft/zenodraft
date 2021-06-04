0e89f91f4623192151a495f05c667dc2e7c96e73
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2021-06-04 19:50:26 +0200

    updated index to make a new draft in a new collection and update its metadata from .zenodo.json

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,7 @@
 import * as dotenv from 'dotenv'
 import { create_empty_deposition } from './create-empty-deposition'
 import { add_file_to_deposition } from './add-file-to-deposition'
-import { publish_draft_deposition } from './publish-draft-deposition'
-import { delete_draft_deposition } from './delete-draft-deposition'
+import { add_metadata_to_deposition } from './add-metadata-to-deposition'
 
 
 (async () => {
@@ -10,17 +9,14 @@ import { delete_draft_deposition } from './delete-draft-deposition'
     const access_token: string = dotenv.config().parsed!.ZENODO_SANDBOX_ACCESS_TOKEN
     const api: string = `https://${sandbox ? 'sandbox.' : ''}zenodo.org/api`
 
-    const id = '831648'
-    await create_empty_deposition(api, access_token)
+    const filenames = [
+        'data.txt',
+        'data2.txt'
+    ]
+    const id = await create_empty_deposition(api, access_token)
+    for (const filename of filenames) {
+        await add_file_to_deposition(api, access_token, id, filename)
+    }
+    await add_metadata_to_deposition(api, access_token, id)
 
-    // const filenames = [
-    //     'data.txt',
-    //     'data2.txt'
-    // ]
-    // const id = await create_empty_deposition(api, access_token)
-    // for (const filename of filenames) {
-    //     await add_file_to_deposition(api, access_token, id, filename)
-    // }
-
-    // await delete_draft_deposition(api, access_token, id)
 })()
