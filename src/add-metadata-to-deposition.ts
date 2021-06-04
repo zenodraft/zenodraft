bf2fb07d2765d1ba88145003519248f7b0cca172
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2021-06-04 19:51:04 +0200

    added function to upload metadata to a deposition

---
+++ src/add-metadata-to-deposition.ts
@@ -0,0 +1,32 @@
+import fetch from 'node-fetch'
+import { RequestInit } from 'node-fetch'
+import * as fs from 'fs'
+import { DepositionsResponse } from './zenodo-response-types'
+
+export const add_metadata_to_deposition = async (api: string, access_token: string, id: string, filename: string = '.zenodo.json'): Promise<void> => {
+    console.log(`adding metadata from ${filename} to deposition with id ${id}...`)
+    const endpoint = `/deposit/depositions/${id}`
+    const method = 'PUT'
+    const headers = {
+        'Authorization': `Bearer ${access_token}`,
+        'Content-Type': 'application/json'
+    }
+    const metadata = JSON.parse(fs.readFileSync(filename, 'utf8'))
+    const init: RequestInit = { method, headers, body: JSON.stringify({metadata}) }
+    let response: any
+    try {
+        response = await fetch(`${api}${endpoint}`, init)
+        if (response.ok !== true) {
+            console.debug(response)
+            throw new Error('Response was not OK')
+        }
+    } catch (e) {
+        throw new Error(`Something went wrong on ${method} to ${api}${endpoint}: ${response.status} - ${response.statusText} \n\n\n ${e}`)
+    }
+
+    try {
+        const deposition: DepositionsResponse = await response.json()
+        console.log(`Updated record ${deposition.record_id}.`)
+    } catch (e) {
+        throw new Error(`Something went wrong while retrieving the json. ${e}`)
+    }}
