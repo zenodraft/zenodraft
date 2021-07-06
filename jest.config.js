1f79c1a29535ad71d6d93002bf79839d831513e0
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2021-07-06 12:00:23 +0200

    reformatted jest config file, added ignore rule

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,15 @@
 module.exports = {
-  "testMatch": [
-    "**/tests/**/*.[jt]s"
-  ],
-  "transform": {
-    "^.+\\.(ts|tsx)$": "ts-jest"
-  }
+    globals: {
+        'ts-jest': {
+            diagnostics: {
+                ignoreCodes: [ 'TS151001' ]
+            }
+        }
+    },
+    testMatch: [
+        "**/tests/**/*.[jt]s"
+    ],
+    transform: {
+        "^.+\\.(ts|tsx)$": "ts-jest"
+    }
 }
