5d87c3f9dd1dfe45d0def7e06f353807cd0c7375
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2021-07-10 15:57:16 +0200

    updated test of deposition show latest because it used to show latest draft, not latest

--- tests/deposition/show/latest.test.ts
+++ tests/deposition/show/latest.test.ts
@@ -12,20 +12,20 @@ afterEach(nock.cleanAll)
 
 describe('deposition show latest', () => {
 
-    test('shows latest draft id for depositions in collection with id \'100\'.', async () => {
+    test('shows latest id for depositions in collection with id \'100\'.', async () => {
         const sandbox = true
         define_token(sandbox, 'faux_zenodo_sandbox_token')
         const access_token = helpers_get_access_token_from_environment(sandbox)
-        const concept_record_id = '100'
-        const draft_id = '101'
+        const concept_record_id = '150'
+        const latest_id = '151'
         const reqheaders = define_reqheaders({json: true, token: access_token})
-        const filename_mock = get_mocked_data(draft_id)
+        const filename_mock = get_mocked_data(latest_id)
         const mocked_server = nock('https://sandbox.zenodo.org/api', { reqheaders })
-            .get(`/deposit/depositions/${draft_id}`)
+            .get(`/deposit/depositions/${latest_id}`)
             .times(1)
             .replyWithFile(200, filename_mock)
         const actual = await deposition_show_latest(access_token, sandbox, concept_record_id)
-        const expected = draft_id
+        const expected = latest_id
         expect(actual).toEqual(expected)
     })
 })
