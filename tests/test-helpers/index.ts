194a144059a251aef0ec99f174a1ab402a19e545
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2021-07-11 13:34:44 +0200

    moved sleep() helper to test-helpers directory

--- tests/test-helpers/index.ts
+++ tests/test-helpers/index.ts
@@ -1,3 +1,4 @@
 export { define_reqheaders } from './define-reqheaders'
 export { define_token } from './define-token'
 export { get_mocked_data } from './get-mocked-data'
+export { sleep } from './sleep'
