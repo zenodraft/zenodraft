928a37d111a64c53767eab7c25529f73ace6d03d
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2022-04-20 22:41:03 +0200

    made autocomplete more complete

--- assets/autocomplete.sh
+++ assets/autocomplete.sh
@@ -1,78 +1,88 @@
 #/usr/bin/env bash
 _zenodraft_completions()
 {
-    local history cur draft_id
+    local history cur draft_id used_options
     # Join the array elements from COMP_WORDS using forward slashes, 
     # chop off the uncompleted command from the right hand side,
-    # remove the sandbox and verbose terms:
-    history=$(echo "${COMP_WORDS[@]}" | sed -e 's# #/#g' | sed -e 's#[^/]*$##g' | sed -e 's#--sandbox/##g' | sed -e 's#-s/##g' | sed -e 's#--verbose/##g' | sed -e 's#-v/##g' | sed -e 's#-sv/##g' | sed -e 's#-vs/##g')
+    # remove the sandbox and verbose and help terms:
+    history=$(echo "${COMP_WORDS[@]}" | sed -e 's# #/#g' | sed -e 's#[^/]*$##g' | sed -e 's#/--help##g' | sed -e 's#/--sandbox##g' | sed -e 's#/-s##g' | sed -e 's#/--verbose##g' | sed -e 's#/-v##g' | sed -e 's#/-sv##g' | sed -e 's#/-vs##g')
 
     # Set $cur to the subcommand that is currently being typed
     cur=${COMP_WORDS[COMP_CWORD]}
 
     draft_id=$(echo "${COMP_WORDS[@]:-1}" | sed -e 's#[^0-9]##g')
 
+    # Take all the words that have been entered so far, 
+    # make them into a string; then use grep with exact matching
+    # to select the options that have been used already;
+    # concatenate them with the pipe symbol while ignoring
+    # the last newline
+    used_options=$(echo "${COMP_WORDS[@]}" | grep -F -e "--help" -e "--sandbox" -e "--verbose" -o | sed -z -e 's#\n#|#g' | sed 's/.$//') 
+
     # switch based on $history
     case ${history} in
         ${COMP_WORDS[0]}/)
-            COMPREPLY=($(compgen -W "deposition file metadata" -- ${cur}))
+            COMPREPLY=($(compgen -X "*($used_options)" -W "--help deposition file metadata" -- ${cur}))
             ;;
         ${COMP_WORDS[0]}/deposition/)
-            COMPREPLY=($(compgen -W "create delete publish show" -- ${cur}))
+            COMPREPLY=($(compgen -X "*($used_options)" -W "--help create delete publish show" -- ${cur}))
             ;;
         ${COMP_WORDS[0]}/deposition/create/)
-            COMPREPLY=($(compgen -W "concept version" -- ${cur}))
+            COMPREPLY=($(compgen -X "*($used_options)" -W "--help concept version" -- ${cur}))
+            ;;
+        ${COMP_WORDS[0]}/deposition/create/concept/)
+            COMPREPLY=($(compgen -X "*($used_options)" -W "--help --sandbox --verbose" -- ${cur}))
             ;;
         ${COMP_WORDS[0]}/deposition/create/version/)
-            COMPREPLY=($(compgen -W "<concept_id>" -- ${cur}))
+            COMPREPLY=($(compgen -X "*($used_options)" -W "--help --sandbox --verbose <concept_id>" -- ${cur}))
             ;;
         ${COMP_WORDS[0]}/deposition/delete/)
-            COMPREPLY=($(compgen -W "<version_id>" -- ${cur}))
+            COMPREPLY=($(compgen -X "*($used_options)" -W "--help --sandbox --verbose <version_id>" -- ${cur}))
             ;;
         ${COMP_WORDS[0]}/deposition/publish/)
-            COMPREPLY=($(compgen -W "<version_id>" -- ${cur}))
+            COMPREPLY=($(compgen -X "*($used_options)" -W "--help --sandbox --verbose <version_id>" -- ${cur}))
             ;;
         ${COMP_WORDS[0]}/deposition/show/)
-            COMPREPLY=($(compgen -W "details draft files latest prereserved" -- ${cur}))
+            COMPREPLY=($(compgen -X "*($used_options)" -W "--help details draft files latest prereserved" -- ${cur}))
             ;;
         ${COMP_WORDS[0]}/deposition/show/details/)
-            COMPREPLY=($(compgen -W "<version_id>" -- ${cur}))
+            COMPREPLY=($(compgen -X "*($used_options)" -W "--help --sandbox --verbose <version_id>" -- ${cur}))
             ;;
         ${COMP_WORDS[0]}/deposition/show/draft/)
-            COMPREPLY=($(compgen -W "<concept_id>" -- ${cur}))
+            COMPREPLY=($(compgen -X "*($used_options)" -W "--help --sandbox --verbose <concept_id>" -- ${cur}))
             ;;
         ${COMP_WORDS[0]}/deposition/show/files/)
-            COMPREPLY=($(compgen -W "<version_id>" -- ${cur}))
+            COMPREPLY=($(compgen -X "*($used_options)" -W "--help --sandbox --verbose <version_id>" -- ${cur}))
             ;;
         ${COMP_WORDS[0]}/deposition/show/latest/)
-            COMPREPLY=($(compgen -W "<concept_id>" -- ${cur}))
+            COMPREPLY=($(compgen -X "*($used_options)" -W "--help --sandbox --verbose <concept_id>" -- ${cur}))
             ;;
         ${COMP_WORDS[0]}/deposition/show/prereserved/)
-            COMPREPLY=($(compgen -W "<version_id>" -- ${cur}))
+            COMPREPLY=($(compgen -X "*($used_options)" -W "--help --sandbox --verbose <version_id>" -- ${cur}))
             ;;
         ${COMP_WORDS[0]}/file/)
-            COMPREPLY=($(compgen -W "add delete" -- ${cur}))
+            COMPREPLY=($(compgen -X "*($used_options)" -W "--help add delete" -- ${cur}))
             ;;
         ${COMP_WORDS[0]}/file/add/)
-            COMPREPLY=($(compgen -W "<version_id>" -- ${cur}))
+            COMPREPLY=($(compgen -X "*($used_options)" -W "--help --sandbox --verbose <version_id>" -- ${cur}))
             ;;
         ${COMP_WORDS[0]}/file/add/${draft_id}/)
             COMPREPLY=($(compgen -o filenames -A file -- ${cur}))
             ;;
         ${COMP_WORDS[0]}/file/delete/)
-            COMPREPLY=($(compgen -W "<version_id>" -- ${cur}))
+            COMPREPLY=($(compgen -X "*($used_options)" -W "--help --sandbox --verbose <version_id>" -- ${cur}))
             ;;
         ${COMP_WORDS[0]}/file/delete/${draft_id}/)
-            COMPREPLY=($(compgen -W "<remote_filename>"))
+            COMPREPLY=($(compgen -X "*($used_options)" -W "--help --sandbox --verbose <remote_filename>"))
             ;;
         ${COMP_WORDS[0]}/metadata/)
-            COMPREPLY=($(compgen -W "clear update" -- ${cur}))
+            COMPREPLY=($(compgen -X "*($used_options)" -W "--help clear update" -- ${cur}))
             ;;
         ${COMP_WORDS[0]}/metadata/clear/)
-            COMPREPLY=($(compgen -W "<version_id>" -- ${cur}))
+            COMPREPLY=($(compgen -X "*($used_options)" -W "--help --sandbox --verbose <version_id>" -- ${cur}))
             ;;
         ${COMP_WORDS[0]}/metadata/update/)
-            COMPREPLY=($(compgen -W "<version_id>" -- ${cur}))
+            COMPREPLY=($(compgen -X "*($used_options)" -W "--help --sandbox --verbose <version_id>" -- ${cur}))
             ;;
         ${COMP_WORDS[0]}/metadata/update/${draft_id}/)
             COMPREPLY=($(compgen -o filenames -A file -- ${cur}))
@@ -82,4 +92,4 @@ _zenodraft_completions()
             ;;
     esac
 }
-complete -F _zenodraft_completions zenodraft
+complete -o nosort -F _zenodraft_completions zenodraft
