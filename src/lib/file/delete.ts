f66abe534d026e3d2eb9b561a6507c49fb859196
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2024-01-31 14:31:28 +0100

    now sending content type headers when deleting a remote file

--- src/lib/file/delete.ts
+++ src/lib/file/delete.ts
@@ -8,21 +8,31 @@ export const file_delete = async (token: string, sandbox: boolean, version_id: s
         console.log(`deleting file ${filename} from deposition with id ${version_id}...`)
     }
     const deposition = await deposition_show_details(token, sandbox, version_id, verbose)
-    const headers = {
-        'Authorization': `Bearer ${token}`,
-        'Content-Type': 'application/json'
-    }
-    const url1 = `${deposition.links.files}`
-    const response1 = await fetch(url1, { method: 'GET', headers })
+    const response1 = await fetch(deposition.links.files, {
+        method: 'GET',
+        headers: {
+            'Authorization': `Bearer ${token}`,
+            'Content-Type': 'application/json'
+        }
+    })
     if (response1.ok !== true) {
-        throw new Error(`Something went wrong on GET to ${url1}: ${response1.status} - ${response1.statusText} `)
+        throw new Error(`Something went wrong on GET to ${deposition.links.files}: ${response1.status} - ${response1.statusText} `)
     }
     const files: File[] = await response1.json()
-    const fileid = files.filter(file => file.filename == filename)[0].id
+    const filtered = files.filter(file => file.filename === filename)
+    if (filtered.length === 0) {
+        throw new Error(`There's no file named '${filename}' in deposition with id ${version_id}.`)
+    }
+    const fileid = filtered[0].id
 
-    const url2 = `${deposition.links.files}/${fileid}`
-    const response2 = await fetch(url2, { method: 'DELETE', headers })
+    const response2 = await fetch(`${deposition.links.files}/${fileid}`, {
+        method: 'DELETE',
+        headers: {
+            'Authorization': `Bearer ${token}`,
+            'Content-Type': 'application/json'
+        }
+    })
     if (response2.ok !== true) {
-        throw new Error(`Something went wrong on DELETE to ${url2}: ${response2.status} - ${response2.statusText} `)
+        throw new Error(`Something went wrong on DELETE to ${deposition.links.files}/${fileid}: ${response2.status} - ${response2.statusText} `)
     }
 }
