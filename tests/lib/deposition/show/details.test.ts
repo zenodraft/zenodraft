dfa0f50247351eddddbd9f5b12a19ad947bc40e3
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2024-02-01 14:48:55 +0100

    updated tests that verify contents of error message

--- tests/lib/deposition/show/details.test.ts
+++ tests/lib/deposition/show/details.test.ts
@@ -50,7 +50,7 @@ describe('deposition show details with expected type \'deposition\' for mock con
         try {
             await throwfun()
         } catch (e) {
-            expect(e.message).toBe("Something went wrong on GET to https://sandbox.zenodo.org/api/deposit/depositions/999: 404 - Not Found")
+            expect(e.message).toBe("(errid 8) Something went wrong on GET to https://sandbox.zenodo.org/api/deposit/depositions/999: 404 - Not Found")
         }
     })
 
@@ -66,7 +66,7 @@ describe('deposition show details with expected type \'deposition\' for mock con
         try {
             await throwfun()
         } catch (e) {
-            expect(e.message).toBe("Something went wrong on GET to https://sandbox.zenodo.org/api/deposit/depositions/100: 404 - Not Found")
+            expect(e.message).toBe("(errid 8) Something went wrong on GET to https://sandbox.zenodo.org/api/deposit/depositions/100: 404 - Not Found")
         }
     })
 
